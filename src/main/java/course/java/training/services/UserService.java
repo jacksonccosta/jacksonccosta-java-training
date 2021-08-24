b5efa0389cc6ea2357eb0e2ec367b19fcbe15757
Jackson Costa <jacksonccosta@yahoo.com>
2021-08-24 16:42:44 -0300

    Implements Insert, Delete, Update in User Entity

--- src/main/java/course/java/training/services/UserService.java
+++ src/main/java/course/java/training/services/UserService.java
@@ -23,4 +23,24 @@ public class UserService {
 		Optional<User> user = userRepository.findById(Id);
 		return user.get();
 	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
